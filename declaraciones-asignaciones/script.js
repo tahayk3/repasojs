4eb01180656aa1f94387ce6ff324e42168caaa91
tahayk3 <tahayk3@gmail.com>
2024-02-21 13:36:02 -0600

    variables y funciones que vienen por defecto en js

---
+++ declaraciones-asignaciones/script.js
@@ -0,0 +1,31 @@
+var nombreVariable = "Tortilla"
+let nombrevariable2 = "pollo"
+const nombrevariable3 = "queso"
+
+let x,y,z;
+x=1;
+y=2;
+z=y-3;
+
+
+
+/*
+Las variables de tipo var, no esta limitadas a un bloque de codigo, se pueden redeclarar(puede ser una fuente de errores)
+
+let y const estan limitadas por algun bloque de codigo
+
+let no se puede redeclarar, pero si reasignar}
+const no puede modificar su valor
+
+cualquiera de las tres puede aceptar datos, de tipo number, string, bool etc
+
+const no se puede ni reasignar y tampoco redeclarar 
+
+const no puede quedar sin valor, let y var si
+
+se recomieda utlizar siempre const, a menos que alguna variable deba ser reasignada, por lo que debe utilizarse let
+
+NO se recomieda utilizar var
+
+js es caseSensitive
+*/
---
+++ funciones/funciones-js.js
@@ -0,0 +1,6 @@
+//parseInt: convierte un texto en un numero entero
+let a = 5;
+let b = "7";
+let c = a + parseInt(b);
+
+console.log(c);
